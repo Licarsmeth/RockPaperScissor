837bf65caa69b19828d0da07e0a143682506d38e
Licar <84367661+Licarsmeth@users.noreply.github.com>
2023-07-30 18:57:01 +0545

    Make the function for one round game

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-
 let getComputerChoice = () => {
-    // generating random number from 0 to 2
+  // generating random number from 0 to 2
   let computerChoice = Math.floor(Math.random() * 3);
-    //changing 0 1 2 to Rock Paper Scissors
-    switch(computerChoice){
-        case 0:
-            return "Rock";
-            break;
-        case 1:
-            return "Paper";
-            break;
-        default:
-            return "Scissors"
-}
+  //changing 0 1 2 to Rock Paper Scissors
+  switch (computerChoice) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    default:
+      return "Scissor";
+  }
 };
 
-console.log(getComputerChoice());
+// console.log(playerChoice);
+// console.log(getComputerChoice());
+
+let playRound = (playerChoice, computerChoice) => {
+  //losing condition
+  if (
+    (playerChoice == "Rock" && computerChoice == "Paper") ||
+    (playerChoice == "Paper" && computerChoice == "Scissor") ||
+    (playerChoice == "Scissor" && computerChoice == "Rock")
+  ) {
+    return `You lose! ${computerChoice} beats ${playerChoice}`;
+  }
+  // draw condition
+  else if (playerChoice == computerChoice) {
+    return `Draw!`;
+  }
+  // winning condition
+  else {
+    return `You win! ${playerChoice} beats ${computerChoice}`;
+  }
+};
+
+const playerChoice = "rocK";
+const computerChoice = getComputerChoice();
+
+console.log(playerChoice);
+console.log(`computer ${computerChoice}`);
+// pass playerchoice as first letter capitalized
+console.log(
+  playRound(
+    playerChoice.slice(0, 1).toUpperCase() +
+      playerChoice.slice(1).toLowerCase(),
+    computerChoice
+  )
+);
